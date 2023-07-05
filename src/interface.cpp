6652702ba85c7a5cb7584564645a7cd47117665a
Gabriella Marimon <marimongabriella@gmail.com>
2023-07-04 21:00:55 -0300

    moving interface implementation to a separate file

--- src/interface.cpp
+++ src/interface.cpp
@@ -1,7 +1,63 @@
 #include "../include/interface.h"
 
-int interface_main()
+Interface::Interface(bool is_participant, list<Work_Station> participants)
 {
-    //Not useful at the moment since interface is a class
-    //Keeping file in case we need it in the future
-}
+    this->is_participant = is_participant;
+    this->participants = participants;
+};
+
+//Get terminal input and validate it according to station (participant or manager)
+void Interface::terminal()
+{
+    bool valid_input = false;
+    
+    while (valid_input == false)
+    {
+        vector<string> input_vec;
+        string terminal_input;
+
+        getline(cin,terminal_input);
+        transform(terminal_input.begin(), terminal_input.end(), terminal_input.begin(), ::toupper);
+
+        int space_position = terminal_input.find(" ");
+        string cmd = terminal_input.substr(0, space_position);
+        string cmd_argument = terminal_input.substr(space_position+1);
+
+        if (this->is_participant)
+        {
+            if (cmd == EXIT)
+                valid_input = true;
+            else
+                cout << "The only valid command for participant station is 'EXIT'" << endl;
+        }
+        else
+        {
+            if (cmd == WAKEUP && cmd_argument != cmd) //if no hostname is informed, no argument will have been found in the terminal command and substrings will be equal
+            {
+                string hostname = cmd_argument;
+
+                //wakeup(hostname,&this->participants); //Not a real method, just writing out program flow
+                show_participants(); //Show updated participants list
+
+                valid_input = true;
+            }
+            else
+                cout << "The only valid command for manager station is 'WAKEUP <hostname>'" << endl;
+        };
+    };
+
+};
+
+void Interface::show_participants()
+{
+    /*
+    * Show participants
+    */
+};
+
+void Interface::show_manager()
+{
+    /*
+    * Show manager station
+    */
+};
