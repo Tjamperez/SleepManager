64e27582dc4eda9ff9949b40f4ab79b7bbd7fe4b
gabriellamarimon <89275621+gabriellamarimon@users.noreply.github.com>
2023-07-12 11:53:21 -0300

    adding services - not working yet due to binding error

--- include/interface.h
+++ include/interface.h
@@ -1,32 +1,31 @@
-#ifndef INTERFACE_H_
-#define INTERFACE_H_
+// #ifndef INTERFACE_H_
+// #define INTERFACE_H_
 
-#include <iostream>
-#include <list>
-#include <string>
-#include <vector>
-#include <algorithm>
-#include <memory>
-#include "../include/work_station.h"
+// #include <iostream>
+// #include <list>
+// #include <string>
+// #include <vector>
+// #include <algorithm>
+// #include <memory>
 
-#define EXIT "EXIT"
-#define WAKEUP "WAKEUP"
-using namespace std;
+// #define EXIT "EXIT"
+// #define WAKEUP "WAKEUP"
+// using namespace std;
 
-class Interface{
-    protected:
-        bool is_participant;
-        shared_ptr<WorkStationTable> participants;
+// class Interface{
+//     protected:
+//         bool is_participant;
+//         shared_ptr<WorkStationTable> participants;
 
-        void show_participants();
-        void show_manager();
+//         void show_participants();
+//         void show_manager();
   
-    public:
-        Interface(
-            bool is_participant,
-            shared_ptr<WorkStationTable> participants
-        );
-        void terminal();
-};
+//     public:
+//         Interface(
+//             bool is_participant,
+//             shared_ptr<WorkStationTable> participants
+//         );
+//         void terminal();
+// };
 
-#endif
+// #endif
