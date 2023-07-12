797054c6f8570442ee69fd454811209919129e7c
gabriellamarimon <89275621+gabriellamarimon@users.noreply.github.com>
2023-07-12 11:51:13 -0300

    Update manager.cpp
    adding services - not working yet due to binding error

--- src/manager.cpp
+++ src/manager.cpp
@@ -1,17 +1,15 @@
-#include "../include/work_station.h"
 #include "../include/manager.h"
-#include "../include/interface.h"
 #include "../include/server.h"
 
 void manager_main(void)
 {
-    Server_Connection server;
-    server.start_server();
+    // Server_Connection server;
+    // server.start_server();
 
-    shared_ptr<WorkStationTable> participants; //Adding this just to test interface. Discovery subservice will eiter return a list of participants or set a global variable
+    // list<Work_Station> participants; //Adding this just to test interface. Discovery subservice will eiter return a list of participants or set a global variable
 
-    bool is_participant = false;
-    Interface interface(is_participant,participants);
+    // bool is_participant = false;
+    // Interface interface(is_participant,participants);
     
-    interface.terminal();
+    // interface.terminal();
 }
