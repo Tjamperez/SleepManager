d2ab5f704888ad2f5ff39cd02957b3dd1fbc785b
gabriellamarimon <89275621+gabriellamarimon@users.noreply.github.com>
2023-07-12 11:54:07 -0300

    adding services - not working yet due to binding error

--- include/server.h
+++ include/server.h
@@ -3,7 +3,6 @@
 
 #include "../include/discovery.h"
 #include "../include/helper.h"
-#include "../include/work_station.h"
 #include <sys/types.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -18,6 +17,8 @@
 #include <sys/socket.h>
 #include <sys/ioctl.h>
 #include <net/if.h>
+#include <map>
+#include <mutex>
 
 #define PORT 4000
 #define BUFFER_SIZE 10000
@@ -29,29 +30,49 @@
 #define FIND_MANAGER "FIND_MANAGER"
 #define FIND_PARTICIPANTS "FIND_PARTICIPANTS"
 #define MANAGER_FOUND "MANAGER_FOUND"
-#define MANAGER "MANAGER"
+#define PARTICIPANT_FOUND "PARTICIPANT_FOUND"
+#define SEND_STATUS_UPDATE "SEND_STATUS_UPDATE"
+#define SENDING_STATUS_UPDATE "SENDING_STATUS_UPDATE"
+#define EXIT "EXIT"
+#define WAKEUP "WAKEUP"
+#define UNKNOWN 0
+#define NO_CHANGE -1
 
 class Server_Connection
 {
     public:
-        Server_Connection();
-        void start_client();
-        void start_server();
+        Server_Connection(bool is_manager);
+        void start();
+        void start_client(uint16_t packet_type,string _payload);
+        void start_server(uint16_t packet_type,string _payload);
 
     protected:
-        clock_t start;//começo do programa
+        //clock_t start;//começo do programa
         struct sockaddr_in serv_addr, from;        
         struct hostent *server;
         uint16_t seqn; //Sequence number used in packets
         string hostname;
-        WorkStation work_station;
+        Work_Station work_station; //work station where program is running
+        map<string,Work_Station> participants; //like a dictionary: <hostname, participant>
+        mutex participants_mutex; //access control
+        bool is_manager;
+        bool active;
+        uint16_t status_update;
 
         bool hasTimeoutPassed( int timeoutInMicroseconds);
-        Packet create_packet(uint16_t type, string _payload, bool is_manager);
+        Packet create_packet(uint16_t type, string _payload);
         string packet_to_string(Packet packet);
         Packet string_to_packet(string packet_str);
         char* get_mac();
         char* get_ip();
+        Work_Station process_message(char packet_string[BUFFER_SIZE]);
+        void add_participant(Work_Station participant);
+        Work_Station create_participant(string hostname, string mac_adress, string ip_address, int status, bool is_manager);
+        void discovery();
+        void monitoring();
+        void interface();
+        void terminal_cmds();
+        void show_participants();
 };
 
 #endif
